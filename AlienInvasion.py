41a7def31fd41a61d4a8e3fd1798b85604a3f265
danels7 <danels7@outlook.com>
2026-04-10 18:47:55 -0400

    basic initialization and mainloop code

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -2,4 +2,17 @@ import pygame
 
 class AlienInvasion:
     def __init__(self):
-        pass
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("Alien Invasion")
+        self.running = False
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # stuff
+
+            pygame.display.flip()
