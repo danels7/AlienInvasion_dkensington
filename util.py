32538557ea3164bec96f6b84b0d2beb6e10dd5b9
danels7 <danels7@outlook.com>
2026-04-11 14:03:45 -0400

    specialize pathing for existing asset directories

--- util.py
+++ util.py
@@ -4,6 +4,23 @@ from pathlib import Path
 CWD = Path.cwd()
 ASSETS = Path.joinpath(CWD, "Assets")
 
+FILE = Path.joinpath(ASSETS, "file")
+FONTS = Path.joinpath(ASSETS, "Fonts")
+IMAGES = Path.joinpath(ASSETS, "images")
+SOUND = Path.joinpath(ASSETS, "sound")
+
 
 def asset_path(subDir: str, fileName: str) -> Path:
     return Path.joinpath(ASSETS, subDir, fileName)
+
+def file_asset(fileName: str) -> Path:
+    return Path.joinpath(FILE, fileName)
+
+def font_asset(fileName: str) -> Path:
+    return Path.joinpath(FONTS, fileName)
+
+def image_asset(fileName: str) -> Path:
+    return Path.joinpath(IMAGES, fileName)
+
+def sound_asset(fileName: str) -> Path:
+    return Path.joinpath(SOUND, fileName)
