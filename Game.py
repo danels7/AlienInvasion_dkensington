1efc8ccee6a359e7e92389c03438509b36a84e38
danels7 <danels7@outlook.com>
2026-04-11 16:28:38 -0400

    load and blit background

--- Game.py
+++ Game.py
@@ -2,12 +2,20 @@ import pygame
 from pygame import Surface
 from pygame.event import Event
 from Ship import Ship
+import util
+
+
+BACKGROUNDASSET = util.image_asset("Starbasesnow.png")
 
 
 class Game:
     def __init__(self, screen: Surface):
         self.screen = screen.subsurface(0, 0, screen.get_width(), screen.get_height())
         self.ship = Ship(self.screen)
+
+        with BACKGROUNDASSET.open() as bg:
+            self.background = pygame.image.load(bg)
+        self.screen.blit(self.background, self.screen.get_rect())
 
     def process_event(self, event: Event, dt: float) -> None:
         if event.type == pygame.KEYDOWN:
