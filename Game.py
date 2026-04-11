f2f893982a5fa3578cd5cea391def97ecae6751a
danels7 <danels7@outlook.com>
2026-04-11 14:55:35 -0400

    placeholer handle_events function

--- Game.py
+++ Game.py
@@ -1,7 +1,12 @@
 import pygame
 from pygame import Surface
+from pygame.event import Event
 
 
 class Game:
     def __init__(self, screen: Surface):
         self.screen = screen.subsurface(0, 0, screen.get_width(), screen.get_height())
+
+    def handle_events(self, events: list[Event]) -> None:
+        for event in events:
+            pass
