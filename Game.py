0a099d937f13b5c30985e7ade5bb382eac02d10a
danels7 <danels7@outlook.com>
2026-04-11 15:48:48 -0400

    create ship in game

--- Game.py
+++ Game.py
@@ -1,11 +1,13 @@
 import pygame
 from pygame import Surface
 from pygame.event import Event
+from Ship import Ship
 
 
 class Game:
     def __init__(self, screen: Surface):
         self.screen = screen.subsurface(0, 0, screen.get_width(), screen.get_height())
+        self.ship = Ship()
 
     def process_event(self, event: Event, dt: float) -> None:
         pass
