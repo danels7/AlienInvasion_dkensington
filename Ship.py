1d546436c4c2b6224a1aeef392eb064bd5e3b693
danels7 <danels7@outlook.com>
2026-04-11 14:50:34 -0400

    load ship image, declare vars for x and y coords

--- Ship.py
+++ Ship.py
@@ -1,3 +1,15 @@
+import pygame
+import util
+
+
+SHIPASSET = util.image_asset("ship2.png")
+
+
 class Ship:
     def __init__(self):
-        pass
+        with SHIPASSET.open() as img:
+            self.shipImg = pygame.image.load(img)
+
+        self.x: int
+        self.y: int
+        
