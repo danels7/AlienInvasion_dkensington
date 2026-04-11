bb0358639870fd1f79c95031f9b07b4ab958ece5
danels7 <danels7@outlook.com>
2026-04-11 15:46:17 -0400

    add basic movement logic to ship

--- Ship.py
+++ Ship.py
@@ -4,12 +4,19 @@ import util
 
 SHIPASSET = util.image_asset("ship2.png")
 
+MOVESPEED = 512 # ship speed in px/sec
+
 
 class Ship:
     def __init__(self):
         with SHIPASSET.open() as img:
             self.shipImg = pygame.image.load(img)
 
-        self.x: int
-        self.y: int
-        
+        self.x: float
+        self.y: float
+
+    def move_right(self, dt: float):
+        self.x += MOVESPEED * dt
+
+    def move_left(self, dt: float):
+        self.x -= MOVESPEED * dt
