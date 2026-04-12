30dc3217a44bbc72b645929471ccee677508f577
danels7 <danels7@outlook.com>
2026-04-12 19:00:07 -0400

    added optional scale multiplier

--- VisualAsset.py
+++ VisualAsset.py
@@ -1,9 +1,15 @@
+import pygame
 from pygame import Surface
 
 
 class VisualAsset:
-    def __init__(self, parentScreen: Surface, img: Surface):
-        self.img = img.copy()
+    def __init__(self, parentScreen: Surface, img: Surface, imgScale: float | None = None):
+        if imgScale is None:
+            self.img = img.copy()
+        else:
+            width = img.get_width() * imgScale
+            height = img.get_height() * imgScale
+            self.img = pygame.transform.scale(img, (width, height))
         self.rect = img.get_rect()
         self.parent = parentScreen
 
