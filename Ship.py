8804573f7e95aec94f489d646d330d7580a1b9a7
danels7 <danels7@outlook.com>
2026-04-12 21:00:06 -0400

    fix up and down being flipped

--- Ship.py
+++ Ship.py
@@ -40,9 +40,9 @@ class Ship(VisualAsset):
         self.rect.x = round(self.x)
 
     def move_up(self, dt: float) -> None:
-        self.y = pygame.math.clamp(self.y - (MOVESPEED * dt), self.yMin, self.yMax)
+        self.y = pygame.math.clamp(self.y + (MOVESPEED * dt), self.yMin, self.yMax)
         self.rect.y = round(self.y)
 
     def move_down(self, dt: float) -> None:
-        self.y = pygame.math.clamp(self.y + (MOVESPEED * dt), self.yMin, self.yMax)
+        self.y = pygame.math.clamp(self.y - (MOVESPEED * dt), self.yMin, self.yMax)
         self.rect.y = round(self.y)
