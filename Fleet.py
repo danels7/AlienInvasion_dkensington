b0280d96cea3832c00bb6c2a25e8df37515c2d81
danels7 <danels7@outlook.com>
2026-04-19 15:05:43 -0400

    create aliens with proper placement and sizing

--- Fleet.py
+++ Fleet.py
@@ -1,7 +1,23 @@
 import pygame
+from pygame import Surface
 from Alien import Alien
 
 
-class Fleet:
-    def __init__(self, width: int, height: int):
+class Fleet():
+    def __init__(self, screen: Surface, fleetWidth: int, fleetHeight: int, alienSpeed: float):
+        offset = 20
+        spawnAreaWidth = screen.get_width() - (offset * 2)
+        spawnAreaHeight = screen.get_height() * 0.45
+
+        dx = spawnAreaWidth / fleetWidth
+        dy = spawnAreaHeight / fleetHeight
+
+        alienHeightDivisor = (screen.get_height() / spawnAreaHeight) * fleetHeight
+
         self.Aliens: list[list[Alien]] = []
+
+        for y in range(fleetWidth):
+            row: list[Alien] = []
+            for x in range(fleetHeight):
+                row.append(Alien(screen, alienSpeed, (x*dx)+offset, (y*dy)+offset, alienHeightDivisor))
+            self.Aliens.append(row)
