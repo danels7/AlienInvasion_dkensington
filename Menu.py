ab31d7fa7ed2472d8d9c7b38add430379a52c6c1
danels7 <danels7@outlook.com>
2026-04-22 11:40:51 -0400

    starting on Menu constructor, added background

--- Menu.py
+++ Menu.py
@@ -1,6 +1,16 @@
+import pygame
+from pygame import Surface
+import util
 
+
+BACKGROUNDASSET = util.image_asset("Starbasesnow.png")
+with BACKGROUNDASSET.open() as img:
+    BACKGROUNDIMG = pygame.image.load(img)
 
 
 class Menu:
-    def __init__(self):
-        pass
+    def __init__(self, screen: Surface):
+        width = screen.get_width()
+        height = screen.get_height()
+        self.screen = screen.subsurface(0, 0, width, height)
+        self.background = BACKGROUNDIMG.copy()
