014d23b00d20bb8fe0e1b6582b884b4b3f86475e
danels7 <danels7@outlook.com>
2026-04-26 09:56:48 -0400

    create play button

--- Menu.py
+++ Menu.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import Surface
+from AlienInvasion import WINDOWWIDTH, WINDOWHEIGHT
 import util
 
 
@@ -8,9 +9,20 @@ with BACKGROUNDASSET.open() as img:
     BACKGROUNDIMG = pygame.image.load(img)
 
 
+pygame.font.init()
+FONTASSET = util.font_asset("Silkscreen-Regular.ttf")
+FONT = pygame.font.Font(FONTASSET, 50)
+
+PLAYBUTTON = FONT.render("PLAY", False, pygame.Color(0xff, 0xff, 0xff), pygame.Color(0x00, 0xff, 0x00))
+PLAYBUTTONRECT = PLAYBUTTON.get_rect()
+PLAYBUTTONRECT.x = round((WINDOWWIDTH/2) - (PLAYBUTTONRECT.width/2))
+PLAYBUTTONRECT.y = round((WINDOWHEIGHT/2) - (PLAYBUTTONRECT.height/2))
+
+
 class Menu:
     def __init__(self, screen: Surface):
         width = screen.get_width()
         height = screen.get_height()
         self.screen = screen.subsurface(0, 0, width, height)
         self.background = BACKGROUNDIMG.copy()
+        self.playButton = PLAYBUTTON.copy()
